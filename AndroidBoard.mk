3a0c2ed8066f96710a7a9da6978ea11a39032011
nobodyAtall <tsogkas@ceid.upatras.gr>
2012-05-26 18:40:46 +0300

    Removing boot files

--- AndroidBoard.mk
+++ AndroidBoard.mk
@@ -13,19 +13,3 @@
 # limitations under the License.
 
 LOCAL_PATH := device/semc/mimmi/
-
-#
-# Boot files
-#
-ifeq ($(TARGET_PREBUILT_KERNEL),)
-TARGET_PREBUILT_KERNEL := $(LOCAL_PATH)/kernel
-endif
-
-#file := $(INSTALLED_KERNEL_TARGET)
-#ALL_PREBUILT += $(file)
-#$(file): $(TARGET_PREBUILT_KERNEL) | $(ACP)
-#	$(transform-prebuilt-to-target)
-
-
-
-

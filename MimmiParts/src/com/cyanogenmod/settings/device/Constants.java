6faf9503473f7dffd50d03c9785784c39a1ecd3c
madbulls <madbulls01@gmail.com>
2012-08-24 19:13:39 +0700

    update to versi 1.2.1

--- MimmiParts/src/com/cyanogenmod/settings/device/Constants.java
+++ MimmiParts/src/com/cyanogenmod/settings/device/Constants.java
@@ -1,5 +1,8 @@
 package com.cyanogenmod.settings.device;
 
+/**
+ * User: Pedro Veloso pedronveloso@androidpt.com
+ */
 public interface Constants {
     
     public static String LOG_TAG = "mimmi_parts";
